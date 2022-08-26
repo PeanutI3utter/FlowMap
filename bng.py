9a0ec69d55ab5507c9424edd4c664dcae1097be3
benjamin-kl <73604811+benjamin-kl@users.noreply.github.com>
2022-08-26 13:34:23 +0200

    blif parser

--- bng.py
+++ bng.py
@@ -1,28 +1,41 @@
 import networkx as nx
-
+import sys
 import pydot
 from networkx.drawing.nx_pydot import graphviz_layout
 from networkx.drawing.nx_pydot import write_dot
-
+import blifparser.blifparser as blifparser
+import os
 
 from enum import Enum
 
 class gate(Enum):
     PI = 0
-    PO = 1
-    AND = 2
-    OR = 3
-    
+    AND = 1
+    OR = 2
+
+
+if (len(sys.argv) != 2):
+    print("Please specify blif input file")
+    sys.exit()
+
+try:
+    infile = open(str(sys.argv[1]), "r")
+    parser = blifparser.BlifParser(str(sys.argv[1]))
+    blif = parser.blif
+    boolfunc = blif.booleanfunctions[0]
+    minterms = boolfunc.truthtable
+except:
+    sys.exit()
     
 nodeid = 0
 bng = nx.Graph()
-infile = open("input.blif", "r")
 
-width = 11
+
+width = len(boolfunc.inputs)
 currentLevel = 0
 
 for primary in range(width):
-    bng.add_node(nodeid, gtype=gate.PI, level=currentLevel, label = str(nodeid))
+    bng.add_node(str(nodeid), gtype=gate.PI, level=currentLevel, label = str(nodeid))
     nodeid += 1
 currentLevel += 1
 
@@ -30,29 +43,31 @@ lastGateInMinterm = []
 
 minterm_spacing = 0;
 lines = infile.readlines()
-for line in lines:
+
+for minterm in minterms:
     currentLevel = 1
-    minterm = int(line, 2)
+    minterm = list(map(int, list(reversed(minterm[:-1]))))
 
     for i in range(width):
         if (i == 1): continue
-        bit = (minterm >> i) & 1
-        bng.add_node(nodeid, gtype=gate.AND, level=currentLevel, label = "AND")
+        
+        bit = minterm[i]
+        bng.add_node(str(nodeid), gtype=gate.AND, level=currentLevel, label = "AND")
         
         if (i == 0):
-            nextbit = (minterm >> 1) & 1
+            nextbit = minterm[1]
             if (bit & nextbit):
                 bng.add_edge(0, nodeid, inverted=False)
                 bng.add_edge(1, nodeid, inverted=False)
             elif (bit):
                 bng.add_edge(0, nodeid, inverted=False)
-                bng.add_edge(1, nodeid, inverted=True, label="~", color="red")
+                bng.add_edge(1, nodeid, inverted=True, color="red")
             elif (nextbit):
-                bng.add_edge(0, nodeid, inverted=True, label="~", color="red")
+                bng.add_edge(0, nodeid, inverted=True, color="red")
                 bng.add_edge(1, nodeid, inverted=False)
             else:
-                bng.add_edge(0, nodeid, inverted=True, label="~", color="red")
-                bng.add_edge(1, nodeid, inverted=True, label="~", color="red")
+                bng.add_edge(0, nodeid, inverted=True, color="red")
+                bng.add_edge(1, nodeid, inverted=True, color="red")
             currentLevel += 1
             nodeid += 1
             continue
@@ -62,7 +77,7 @@ for line in lines:
             bng.add_edge(i, nodeid, inverted=False)
         else:
             bng.add_edge(nodeid - 1, nodeid, inverted=False)
-            bng.add_edge(i, nodeid, inverted=True, label="~", color="red")
+            bng.add_edge(i, nodeid, inverted=True, color="red")
             
         nodeid += 1
         currentLevel += 1
@@ -75,18 +90,19 @@ currentLevel = 20#need max
 for i in range(len(lastGateInMinterm)):
     if (i == 1): continue
     if (i == 0):
-        bng.add_node(nodeid, gtype=gate.OR, level = currentLevel, label = "OR")
+        bng.add_node(str(nodeid), gtype=gate.OR, level = currentLevel, label = "OR")
         bng.add_edge(lastGateInMinterm[0], nodeid, inverted=False)
         bng.add_edge(lastGateInMinterm[1], nodeid, inverted=False)
         nodeid += 1
         currentLevel += 1
-        #i += 2
         continue
-    bng.add_node(nodeid, gtype=gate.OR, level = currentLevel, label = "OR")
+    bng.add_node(str(nodeid), gtype=gate.OR, level = currentLevel, label = "OR")
     bng.add_edge(nodeid - 1, nodeid, inverted=False)
     bng.add_edge(lastGateInMinterm[i], nodeid, inverted=False)
     nodeid += 1
     currentLevel += 1
+
+print("Nodes in G: ", bng.nodes(data=True))
 
 write_dot(bng, 'graph.dot')
 
@@ -103,6 +119,7 @@ dotout.write("}" + "\n")
 dotout.write("}")
     
 
-
+dot.close()
+os.remove("graph.dot")
 dotout.close()
 infile.close()
