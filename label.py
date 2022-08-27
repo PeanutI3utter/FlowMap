a5e616b363d41d253e6b5cf14e916c22639666d0
Si Jun Kwon <sijunkwon@gmail.com>
2022-08-27 12:48:44 +0200

    implemented flowgraph generation

--- label.py
+++ label.py
@@ -6,3 +6,45 @@ def extract_subgraph_from (graph: nx.DiGraph, node: str) -> nx.DiGraph:
     sub.append(node)
     return graph.subgraph(sub)
 
+
+def flowGraph(graph: nx.DiGraph):
+    flowgraph = nx.DiGraph()
+    nodes_sorted = list(nx.topological_sort(graph))
+
+    input_nodes = [node for node, deg in graph.in_degree() if not deg]
+    output = nodes_sorted[-1]
+
+    highest_level = graph.nodes[nodes_sorted[-2]]['level']
+    highest_level_nodes = [nodes_sorted[-2]]
+    for node in nodes_sorted[-3::-1]:
+        if graph.nodes[node]['level'] == highest_level:
+            highest_level_nodes.append(node)
+        else:
+            break
+
+    inter_nodes = nodes_sorted[len(input_nodes):-len(highest_level_nodes)-1]
+    
+    for node in input_nodes:
+        flowgraph.add_edge('S', node + '_in')
+        flowgraph.add_edge(node + '_in', node + '_out', capacity=1)
+    
+    for node in inter_nodes:
+        incoming_edges = graph.in_edges(node)
+        for edge in incoming_edges:
+            flowgraph.add_edge(edge[0] + '_out', node + '_in')
+        flowgraph.add_edge(node + '_in', node + '_out', capacity=1)
+    
+    for node in highest_level_nodes:
+        incoming_edges = graph.in_edges(node)
+        for edge in incoming_edges:
+            if edge[0] not in highest_level_nodes:
+                flowgraph.add_edge(edge[0] + '_out', f'DRAIN_{output}')
+
+    for edge in graph.in_edges(output):
+        if edge[0] not in highest_level_nodes:
+            flowgraph.add_edge(edge[0] + '_out', f'DRAIN_{output}')
+
+    nx.set_node_attributes(flowgraph, {f'DRAIN_{output}': {'contains': highest_level_nodes, 'mapped_to': output}})
+
+    return flowgraph
+        
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 from draw import draw_digraph
-from label import extract_subgraph_from
+from label import extract_subgraph_from, flowGraph
 
 
 class tcolors:
@@ -86,9 +86,34 @@ TEST_SET_SUBGRAPH = [
 ]
 
 
+TEST_SET_FLOWGRAPH = [
+    {
+        'graph': G,
+        'expected_nodes': [
+            'S', 'a_in', 'a_out', 'b_in', 'b_out', 'c_in', 'c_out',
+            'd_in', 'd_out', 'e_in', 'e_out', 'f_in', 'f_out',
+            'g_in', 'g_out', 'h_in', 'h_out', 'i_in', 'i_out', ('DRAIN_m', {'contains': ['l', 'k', 'j'], 'mapped_to': 'm'})
+        ],
+        'expected_edges': [
+            ('a_in', 'a_out', {'capacity': 1}), ('b_in', 'b_out', {'capacity': 1}),
+            ('c_in', 'c_out', {'capacity': 1}), ('d_in', 'd_out', {'capacity': 1}),
+            ('e_in', 'e_out', {'capacity': 1}), ('f_in', 'f_out', {'capacity': 1}),
+            ('g_in', 'g_out', {'capacity': 1}), ('h_in', 'h_out', {'capacity': 1}),
+            ('i_in', 'i_out', {'capacity': 1}),
+            ('S', 'a_in'), ('S', 'b_in'), ('S', 'c_in'),
+            ('S', 'd_in'), ('S', 'e_in'),
+            ('a_out', 'f_in'), ('b_out', 'f_in'), ('b_out', 'g_in'),
+            ('c_out', 'g_in'), ('d_out', 'h_in'), ('e_out', 'h_in'),
+            ('f_out', 'i_in'), ('g_out', 'i_in'),
+            ('h_out', 'DRAIN_m'), ('i_out', 'DRAIN_m'), ('g_out', 'DRAIN_m'),
+            ('f_out', 'DRAIN_m')
+        ]
+    },
+]
+
 
 def test_subgraph(test_set: dict):
-    print("Starting subgraph test..")
+    print('Starting subgraph test..')
     for i, test_data in enumerate(test_set):
         (graph, node, expected_nodes, expected_edges) = test_data.values()
         subgraph = extract_subgraph_from(graph, node)
@@ -97,10 +122,25 @@ def test_subgraph(test_set: dict):
         expected_graph.add_edges_from(expected_edges)
 
         if (nx.utils.graphs_equal(subgraph, expected_graph)):
-            print(f"[SUBGRAPH TEST {i + 1}/{len(test_set)}] " + tcolors.OKGREEN + "passed" + tcolors.ENDC)
+            print(f'[SUBGRAPH TEST {i + 1}/{len(test_set)}] ' + tcolors.OKGREEN + 'passed' + tcolors.ENDC)
         else:
-            print(f"[SUBGRAPH TEST {i + 1}/{len(test_set)}] " + tcolors.WARNING + "failed" + tcolors.ENDC)
+            print(f'[SUBGRAPH TEST {i + 1}/{len(test_set)}] ' + tcolors.WARNING + 'failed' + tcolors.ENDC)
 
+
+def test_flowgraph(test_set):
+    print('Starting flowgraph test..')
+    for i, test_data in enumerate(test_set):
+        (graph, expected_nodes, expected_edges) = test_data.values()
+        flowgraph = flowGraph(graph)
+        expected_graph = nx.DiGraph()
+        expected_graph.add_nodes_from(expected_nodes)
+        expected_graph.add_edges_from(expected_edges)
+
+        if (nx.utils.graphs_equal(flowgraph, expected_graph)):
+            print(f'[FLOWGRAPH TEST {i + 1}/{len(test_set)}] ' + tcolors.OKGREEN + 'passed' + tcolors.ENDC)
+        else:
+            print(f'[FLOWGRAPH TEST {i + 1}/{len(test_set)}] ' + tcolors.WARNING + 'failed' + tcolors.ENDC)
 
 
 test_subgraph(TEST_SET_SUBGRAPH)
+test_flowgraph(TEST_SET_FLOWGRAPH)
