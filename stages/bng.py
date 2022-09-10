b1afe0106a6ea105e9bdeda0fe08f7643ab941f1
Si Jun Kwon <sijunkwon@gmail.com>
2022-09-10 16:25:37 +0200

    improved blif_to_bng (height optimised)

--- stages/bng.py
+++ stages/bng.py
@@ -1,113 +1,128 @@
+import blifparser.blifparser as blifparser
 import networkx as nx
 import sys
-import pydot
-from networkx.drawing.nx_pydot import graphviz_layout
-from networkx.drawing.nx_pydot import write_dot
-import blifparser.blifparser as blifparser
-import os
 
 from enum import IntEnum
+from typing import Dict, List, Tuple
+
 
 class gate(IntEnum):
     PI = 0
     AND = 1
     OR = 2
-    LUT = 3
+    PO = 3
+    INTERMEDIATE = 4
+    LUT = 5
 
 
-def blif_to_bng(blif_path: str):
+def blif_to_bng(path_to_blif: str) -> nx.DiGraph:
 
     try:
-        parser = blifparser.BlifParser(blif_path)
-        blif = parser.blif
-        boolfunc = blif.booleanfunctions[0]
-        minterms = boolfunc.truthtable
-        inputnames = boolfunc.inputs
-        outputnames = boolfunc.output
-    except:
+        blif = blifparser.BlifParser(path_to_blif).blif
+    except Exception:
         raise RuntimeError("Error while opening/reading BLIF file.")
-        
-    nodeid = 0
+
+    if len(blif.latches) > 0 or blif.fsm.ispresent:
+        raise RuntimeError("Complex Logic Modules not supported yet")
+
     bng = nx.DiGraph()
+    for input in blif.inputs.inputs:
+        bng.add_node(input, gtype=gate.PI, label=input)
 
+    for output in blif.outputs.outputs:
+        bng.add_node(output, gtype=gate.PO, label=output)
 
-    width = len(boolfunc.inputs)
-    currentLevel = 0
+    and_lookup: Dict[Tuple[bool, str], Dict[Tuple[bool, str], str]] = {}
+    or_lookup: Dict[str, Dict[str, str]] = {}
 
-    for _ in range(width):
-        bng.add_node(nodeid, gtype=gate.PI, level=currentLevel, label = nodeid)
-        nodeid += 1
-    currentLevel += 1
+    and_counter = 0
+    or_counter = 0
 
-    lastGateInMinterm = []
+    for bool_func in blif.booleanfunctions:
+        truth_table = bool_func.truthtable
+        inputs = bool_func.inputs
+        num_input = len(inputs)
 
-    for minterm in minterms:
-        currentLevel = 1
-        minterm = list(map(int, list(reversed(minterm[:-1]))))
-
-        for i in range(width):
-            if (i == 1): continue
-            
-            bit = minterm[i]
-            bng.add_node(nodeid, gtype=gate.AND, level=currentLevel, label = "AND")
-            
-            if (i == 0):
-                nextbit = minterm[1]
-                if (bit & nextbit):
-                    bng.add_edge(0, nodeid, inverted=False)
-                    bng.add_edge(1, nodeid, inverted=False)
-                elif (bit):
-                    bng.add_edge(0, nodeid, inverted=False)
-                    bng.add_edge(1, nodeid, inverted=True, color="red")
-                elif (nextbit):
-                    bng.add_edge(0, nodeid, inverted=True, color="red")
-                    bng.add_edge(1, nodeid, inverted=False)
-                else:
-                    bng.add_edge(0, nodeid, inverted=True, color="red")
-                    bng.add_edge(1, nodeid, inverted=True, color="red")
-                currentLevel += 1
-                nodeid += 1
+        ORs: List[str] = []
+        for minterm in truth_table:
+            if minterm[-1] != '1':
                 continue
-             
-            if (bit == 1):                
-                bng.add_edge(nodeid - 1, nodeid, inverted=False)
-                bng.add_edge(i, nodeid, inverted=False)
+
+            ANDs = []
+            for i in range(num_input):
+                if inputs[i] not in bng.nodes:
+                    bng.add_node(
+                        inputs[i], gtype=gate.INTERMEDIATE, label=inputs[i]
+                    )
+
+                if minterm[i] == '1':
+                    ANDs.append((False, inputs[i]))
+                elif minterm[i] == '0':
+                    ANDs.append((True, inputs[i]))
+
+            while len(ANDs) > 1:
+                left: Tuple[bool, str] = ANDs.pop(0)
+                right: Tuple[bool, str] = ANDs.pop(0)
+
+                if left not in and_lookup:
+                    and_lookup[left] = {}
+
+                if right not in and_lookup[left]:
+
+                    new_and_node = f'AND[{and_counter}]'
+                    and_lookup[left][right] = new_and_node
+                    bng.add_node(
+                        new_and_node, gtype=gate.AND, label=new_and_node
+                    )
+                    bng.add_edge(
+                        left[1], new_and_node, inverted=left[0],
+                        color='purple' if left[0] else ''
+                    )
+                    bng.add_edge(
+                        right[1], new_and_node, inverted=right[0],
+                        color='purple' if right[0] else ''
+                    )
+                    ANDs.append((False, new_and_node))
+                    and_counter += 1
+                else:
+                    ANDs.append((False, and_lookup[left][right]))
+
+            ORs.append(ANDs[0])
+
+        while len(ORs) > 1:
+            left: str = ORs.pop(0)
+            right: str = ORs.pop(0)
+
+            if left[1] not in or_lookup:
+                or_lookup[left[1]] = {}
+
+            if right[1] not in or_lookup[left[1]]:
+
+                new_or_node = f'OR[{or_counter}]'
+                or_lookup[left[1]][right[1]] = new_or_node
+                bng.add_node(new_or_node, gtype=gate.OR, label=new_or_node)
+                bng.add_edge(left[1], new_or_node, inverted=left[0], color='')
+                bng.add_edge(
+                    right[1], new_or_node, inverted=right[0], color=''
+                )
+                ORs.append((False, new_or_node))
+                or_counter += 1
             else:
-                bng.add_edge(nodeid - 1, nodeid, inverted=False)
-                bng.add_edge(i, nodeid, inverted=True, color="red")
-                
-            nodeid += 1
-            currentLevel += 1
-            
-        if (i == width - 1):
-            lastGateInMinterm.append(nodeid - 1)
+                ORs.append((False, or_lookup[left[1]][right[1]]))
 
-    currentLevel = 20#need max
-    for i in range(len(lastGateInMinterm)):
-        if (i == 1): continue
-        if (i == 0):
-            bng.add_node(nodeid, gtype=gate.OR, level = currentLevel, label = "OR")
-            bng.add_edge(lastGateInMinterm[0], nodeid, inverted=False)
-            bng.add_edge(lastGateInMinterm[1], nodeid, inverted=False)
-            nodeid += 1
-            currentLevel += 1
-            continue
-        bng.add_node(nodeid, gtype=gate.OR, level = currentLevel, label = "OR")
-        bng.add_edge(nodeid - 1, nodeid, inverted=False)
-        bng.add_edge(lastGateInMinterm[i], nodeid, inverted=False)
-        nodeid += 1
-        currentLevel += 1
+        bng.add_edge(
+            ORs[0][1], bool_func.output, inverted=ORs[0][0],
+            color='purple' if ORs[0][0] else ''
+        )
 
-    return (bng, inputnames, outputnames)
-
-
+    return bng
 
 
 def bng_to_blif(bng, inputnames, outputnames):
 
     try:
         outputf = open("output.blif", "w")
-    except:
+    except Exception:
         raise RuntimeError("Error while writing output BLIF file.")
 
     nodes = list(nx.topological_sort(bng))
@@ -116,20 +131,22 @@ def bng_to_blif(bng, inputnames, outputnames):
     for inputname in inputnames:
         outputf.write(inputname + " ")
     outputf.write("\n.outputs " + outputnames + "\n\n")
-    
+
     for node in nodes:
         if len(list(bng.predecessors(nodes[node]))) > 0:
-            inpts = list(bng.in_edges(node, data = False))
+            inpts = list(bng.in_edges(node, data=False))
             inpts_str = ""
             for inp in inpts:
                 if inp[0] < len(inputnames):
                     inpts_str += inputnames[inp[0]] + " "
                 else:
-                    inpts_str += "w" + str(inp[0]) + " " 
-            if len(list(bng.out_edges(node, data = False))) == 0:
+                    inpts_str += "w" + str(inp[0]) + " "
+            if len(list(bng.out_edges(node, data=False))) == 0:
                 outputf.write(".names " + inpts_str + outputnames + "\n")
             else:
-                outputf.write(".names " + inpts_str + "w" + str(inpts[0][1]) + "\n")
+                outputf.write(
+                    ".names " + inpts_str + "w" + str(inpts[0][1]) + "\n"
+                )
 
             for cube in onSets[node]:
                 cubestr = ""
@@ -146,19 +163,16 @@ def bng_to_blif(bng, inputnames, outputnames):
 
     outputf.write(".end")
     outputf.close()
-            
-            
 
 
-
-#positional cube notation
+# positional cube notation
 class inSymbol(IntEnum):
     INV = 0
     ON = 1
     OFF = 2
     DC = 3
 
-    
+
 def handleAND(lhs, rhs):
     conjunction = []
     for entry_l in lhs:
@@ -186,17 +200,20 @@ def negateCubes(onSet):
     for cube in range(len(onSet)):
         negatedCube = []
         for symbol in range(len(onSet[cube])):
-            negatedCube.append(inSymbol.ON if onSet[cube][symbol] == inSymbol.OFF else
-                           inSymbol.OFF if onSet[cube][symbol] == inSymbol.ON else
-                           inSymbol.DC)
+            negatedCube.append(
+                inSymbol.ON if onSet[cube][symbol] == inSymbol.OFF else
+                inSymbol.OFF if onSet[cube][symbol] == inSymbol.ON else
+                inSymbol.DC
+            )
+
         negated.append(negatedCube)
     return negated
 
-    
+
 def subgraphToOnSet(subgraph):
     nodes = list(nx.topological_sort(subgraph))
     inputs = []
-    output= -1
+    output = -1
 
     for node in range(len(nodes)):
         if len(list(subgraph.predecessors(nodes[node]))) == 0:
@@ -208,30 +225,28 @@ def subgraphToOnSet(subgraph):
         if len(list(subgraph.predecessors(nodes[node]))) == 0:
             inputOnSet = [inSymbol.DC] * len(inputs)
             inputOnSet[node] = inSymbol.ON
-            nx.set_node_attributes(subgraph, {node:{"onSet":[inputOnSet]}})
+            nx.set_node_attributes(subgraph, {node: {"onSet": [inputOnSet]}})
             continue
         else:
             onSets = nx.get_node_attributes(subgraph, "onSet")
-            incomingEdges = list(subgraph.in_edges(node, data = True))
+            incomingEdges = list(subgraph.in_edges(node, data=True))
             leftParent = incomingEdges[0][0]
             rightParent = incomingEdges[1][0]
-            leftEdgeInverted = incomingEdges[0][2].get("inverted") == True
-            rightEdgeInverted = incomingEdges[1][2].get("inverted") == True
-            leftOnSet = negateCubes(onSets[leftParent]) if leftEdgeInverted else onSets[leftParent]
-            rightOnSet = negateCubes(onSets[rightParent]) if rightEdgeInverted else onSets[rightParent]
+            leftEdgeInverted = bool(incomingEdges[0][2].get("inverted"))
+            rightEdgeInverted = bool(incomingEdges[1][2].get("inverted"))
+            leftOnSet = negateCubes(onSets[leftParent]) \
+                if leftEdgeInverted else onSets[leftParent]
+            rightOnSet = negateCubes(onSets[rightParent]) \
+                if rightEdgeInverted else onSets[rightParent]
             gateTypes = nx.get_node_attributes(subgraph, "gtype")
             ownGate = gateTypes[nodes[node]]
-            if(ownGate == gate.AND):
+            if (ownGate == gate.AND):
                 ownOnSet = handleAND(leftOnSet, rightOnSet)
-            elif(ownGate == gate.OR):
+            elif (ownGate == gate.OR):
                 ownOnSet = handleOR(leftOnSet, rightOnSet)
             else:
                 raise RuntimeError("unexpected gate type")
                 sys.exit()
-            nx.set_node_attributes(subgraph, {node:{'onSet':ownOnSet}})
+            nx.set_node_attributes(subgraph, {node: {'onSet': ownOnSet}})
         if (node == output):
             return ownOnSet
-
-    
-
-    
