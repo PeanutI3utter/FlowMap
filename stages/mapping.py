7deb596f50493d00f428fdeda21f4c8f525dcf95
Si Jun Kwon <sijunkwon@gmail.com>
2022-09-10 16:26:14 +0200

    implemented mapping phase

--- stages/mapping.py
+++ stages/mapping.py
@@ -1,12 +1,115 @@
+import boolexpr as bx
 import networkx as nx
 
+from stages.bng import gate, inSymbol
 
-#this is based on the assumption that the order of incoming edges always coincides with
-#the order of the input variables in the onSet of a LUT
-def merge_LUTs(subgraph: nx.DiGraph, graph: nx.DiGraph, k: int) -> nx.DiGraph: #k probably not needed
+
+def flowmap(labeled_graph: nx.DiGraph) -> nx.DiGraph:
+    LUT_graph = nx.DiGraph()
+    circuit_output = [
+        node for node, deg in labeled_graph.out_degree() if not deg
+    ][0]
+    circuit_inputs = [
+        node for node, deg in labeled_graph.in_degree() if not deg
+    ]
+
+    to_be_mapped = [circuit_output]
+
+    LUT_graph.add_node(circuit_output, label=circuit_output, gtype=gate.PO)
+    for circuit_input in circuit_inputs:
+        LUT_graph.add_node(
+            circuit_input, func=bx.get_var(circuit_input),
+            label=circuit_input, gtype=gate.PI
+        )
+
+    while len(to_be_mapped) > 0:
+        LUT_output = to_be_mapped.pop(0)
+        LUT_nodes = list(nx.topological_sort(labeled_graph.subgraph(
+            node for node in labeled_graph.nodes[LUT_output]['cut']
+            if node not in circuit_inputs
+        )))
+
+        func_stack = {}
+
+        for LUT_node in LUT_nodes:
+            inputs = [i for i, _ in labeled_graph.in_edges(LUT_node)]
+
+            if (labeled_graph.nodes[LUT_node]['gtype'] == gate.AND
+                    or labeled_graph.nodes[LUT_node]['gtype'] == gate.OR):
+
+                assert len(inputs) == 2
+                if inputs[0] not in LUT_nodes:
+                    a = bx.get_var(inputs[0])
+                    func_stack[inputs[0]] = a
+                    if inputs[0] not in LUT_graph.nodes:
+                        LUT_graph.add_node(
+                            inputs[0], label=inputs[0], gtype=gate.LUT
+                        )
+                        to_be_mapped.append(inputs[0])
+                    LUT_graph.add_edge(inputs[0], LUT_output)
+
+                else:
+                    a = func_stack[inputs[0]]
+
+                if inputs[1] not in LUT_nodes:
+                    b = bx.get_var(inputs[1])
+                    func_stack[inputs[1]] = b
+                    if inputs[1] not in LUT_graph.nodes:
+                        LUT_graph.add_node(
+                            inputs[1], label=inputs[1], gtype=gate.LUT
+                        )
+                        to_be_mapped.append(inputs[1])
+
+                    LUT_graph.add_edge(inputs[1], LUT_output)
+                else:
+                    b = func_stack[inputs[1]]
+
+                if labeled_graph.edges[inputs[0], LUT_node]['inverted']:
+                    a = ~a
+
+                if labeled_graph.edges[inputs[1], LUT_node]['inverted']:
+                    b = ~b
+
+                if labeled_graph.nodes[LUT_node]['gtype'] == gate.OR:
+                    func_stack[LUT_node] = a | b
+                else:
+                    func_stack[LUT_node] = a & b
+
+            else:
+                assert len(inputs) == 1
+                if inputs[0] not in LUT_nodes:
+                    a = bx.get_var(inputs[0])
+                    func_stack[inputs[0]] = a
+                    if inputs[0] not in LUT_graph.nodes:
+                        LUT_graph.add_node(
+                            inputs[0], label=inputs[0], gtype=gate.LUT
+                        )
+                        to_be_mapped.append(inputs[0])
+                    LUT_graph.add_edge(inputs[0], LUT_output)
+                else:
+                    a = func_stack[inputs[0]]
+
+                if labeled_graph.edges[inputs[0], LUT_node]['inverted']:
+                    func_stack[LUT_node] = ~a
+                else:
+                    func_stack[LUT_node] = a
+
+        LUT_graph.nodes[LUT_output]['func'] = func_stack[LUT_output]
+
+    return LUT_graph
+
+
+# this is based on the assumption that the order of incoming
+# edges always coincides with the order of the input variables
+# in the onSet of a LUT
+def merge_LUTs(subgraph: nx.DiGraph, graph: nx.DiGraph) \
+        -> nx.DiGraph:
+
     nodes = list(nx.topological_sort(subgraph))
-    #make list from set
-    inputs = list({node for node in graph.nodes if graph.out_edges(node)[1] in nodes})
+    # make list from set
+    inputs = list(
+        {node for node in graph.nodes if graph.out_edges(node)[1] in nodes}
+    )
 
     onSets = nx.get_node_attributes(nodes, "onSet")
     extendedOnSets = []
@@ -14,39 +117,38 @@ def merge_LUTs(subgraph: nx.DiGraph, graph: nx.DiGraph, k: int) -> nx.DiGraph: #
     for node in range(len(nodes)):
         if len(list(subgraph.predecessors(nodes[node]))) == 0:
             onSet = onSets[node]
-            incomingEdges = list(graph.in_edges(node, data = False))
+            incomingEdges = list(graph.in_edges(node, data=False))
 
-            #determine which inputs are connected to the current (upper) LUT
+            # determine which inputs are connected to the current (upper) LUT
             connectedPositions = [False] * len(inputs)
             for inEdge in incomingEdges:
                 position = inputs.index(inEdge[1])
                 connectedPositions[position] = True
-            #extend truth table/onSet with DC based on connected positions
+            # extend truth table/onSet with DC based on connected positions
             newOnSet = []
             for cube in onSet:
                 newCube = []
                 offset = 0
                 for i in range(len(inputs)):
                     if connectedPositions[i]:
-                        #keep old cube value (which is at i - offset)
+                        # keep old cube value (which is at i - offset)
                         newCube.append(cube[i - offset])
                     else:
-                        #add new DC value
+                        # add new DC value
                         newCube.append(inSymbol.DC)
                         offset += 1
                 newOnSet.append(newCube)
             extendedOnSets.append(newOnSet)
-            
-            
-    #merge the extendedOnSets based on onSet of lower LUT
-    #todo: check if the order of inputs in the lower onSet is the same as the node order, otherwise use edges
-    #to get the correct order and reorder the elements of extendedOnSets accordingly
+
+    # merge the extendedOnSets based on onSet of lower LUT
+    # todo: check if the order of inputs in the lower onSet
+    # is the same as the node order, otherwise use edges
+    # to get the correct order and reorder the elements of
+    # extendedOnSets accordingly
     lowerOnSet = onSets[nodes[-1]]
-    width = len(lowerOnSet[0]) 
     for cube in lowerOnSet:
         mergeOnSets(extendedOnSets, cube)
 
+
 def mergeOnSets(onSets, cube):
     return
-    
-                
---
+++ tests/test_map.py
@@ -0,0 +1,121 @@
+import boolexpr as bx
+import networkx as nx
+
+from ..stages.bng import blif_to_bng, gate
+from ..stages.label import label
+from ..stages.mapping import flowmap
+
+
+expected = nx.DiGraph()
+
+
+expected.add_nodes_from([
+    ('F', {
+        'label': 'F',
+        'func': bx.get_var('OR[1]')
+                 | bx.get_var('AND[10]')
+                 | bx.get_var('OR[0]'),
+        'gtype': gate.PO
+    }),
+    ('AND[10]', {
+        'label': 'AND[10]',
+        'func': bx.get_var('AND[8]') & bx.get_var('AND[9]'),
+        'gtype': gate.LUT
+    }),
+    ('OR[0]', {
+        'label': 'OR[0]',
+        'func': (bx.get_var('AND[0]') & bx.get_var('AND[1]')) |
+                (bx.get_var('AND[0]') & bx.get_var('AND[3]')),
+        'gtype': gate.LUT
+    }),
+    ('OR[1]', {
+        'label': 'OR[1]',
+        'func': (bx.get_var('AND[5]') & bx.get_var('AND[1]')) |
+                (bx.get_var('AND[5]') & bx.get_var('AND[3]')),
+        'gtype': gate.LUT
+    }),
+    ('AND[8]', {
+        'label': 'AND[8]',
+        'func': ~bx.get_var('a') & bx.get_var('b'),
+        'gtype': gate.LUT
+    }),
+    ('AND[9]', {
+        'label': 'AND[9]',
+        'func': bx.get_var('c') & ~bx.get_var('d'),
+        'gtype': gate.LUT
+    }),
+    ('AND[0]', {
+        'label': 'AND[0]',
+        'func': bx.get_var('a') & ~bx.get_var('b'),
+        'gtype': gate.LUT
+    }),
+    ('AND[1]', {
+        'label': 'AND[1]',
+        'func': ~bx.get_var('c') & ~bx.get_var('d'),
+        'gtype': gate.LUT
+    }),
+    ('AND[3]', {
+        'label': 'AND[3]',
+        'func': bx.get_var('c') & bx.get_var('d'),
+        'gtype': gate.LUT
+    }),
+    ('AND[5]', {
+        'label': 'AND[5]',
+        'func': bx.get_var('a') & bx.get_var('b'),
+        'gtype': gate.LUT
+    }),
+    ('AND[5]', {
+        'label': 'AND[5]',
+        'func': bx.get_var('a') & bx.get_var('b'),
+        'gtype': gate.LUT
+    }),
+    ('a', {
+        'label': 'a',
+        'func': bx.get_var('a'),
+        'gtype': gate.PI
+    }),
+    ('b', {
+        'label': 'b',
+        'func': bx.get_var('b'),
+        'gtype': gate.PI
+    }),
+    ('c', {
+        'label': 'c',
+        'func': bx.get_var('c'),
+        'gtype': gate.PI
+    }),
+    ('d', {
+        'label': 'd',
+        'func': bx.get_var('d'),
+        'gtype': gate.PI
+    })
+])
+
+
+expected.add_edges_from([
+    ('OR[1]', 'F'), ('AND[10]', 'F'), ('OR[0]', 'F'),
+    ('AND[8]', 'AND[10]'), ('AND[9]', 'AND[10]'),
+    ('AND[0]', 'OR[0]'), ('AND[1]', 'OR[0]'), ('AND[3]', 'OR[0]'),
+    ('AND[1]', 'OR[1]'), ('AND[3]', 'OR[1]'), ('AND[5]', 'OR[1]'),
+    ('a', 'AND[8]'), ('b', 'AND[8]'),
+    ('c', 'AND[9]'), ('d', 'AND[9]'),
+    ('a', 'AND[0]'), ('b', 'AND[0]'),
+    ('c', 'AND[1]'), ('d', 'AND[1]'),
+    ('c', 'AND[3]'), ('d', 'AND[3]'),
+    ('a', 'AND[5]'), ('b', 'AND[5]'),
+])
+
+
+def test_map0():
+    res = flowmap(label(blif_to_bng("tests/input_test.blif"), 3))
+    for node in res.nodes:
+        assert node in expected.nodes
+        assert res.nodes[node]['label'] == expected.nodes[node]['label']
+        assert res.nodes[node]['gtype'] == expected.nodes[node]['gtype']
+        assert res.nodes[node]['func'].equiv(expected.nodes[node]['func'])
+
+    for edge in res.edges:
+        assert edge in expected.edges
+
+    for edge in expected.edges:
+        assert edge in res.edges
