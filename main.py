2327ec7f4e9578b510d9868efd5d59acd6f76239
Si Jun Kwon <sijunkwon@gmail.com>
2022-09-14 11:34:04 +0200

    more verbose output in main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import argparse
+import networkx as nx
 import time
 
-from stages.packing import gate_decomposition, flow_pack
+from stages.enums import gate
 from stages.bng import blif_to_bng
 from stages.bng import bng_to_blif
 from stages.label import label
 from stages.mapping import flowmap
+from stages.packing import gate_decomposition, flow_pack
 
 
 def main():
@@ -71,16 +73,30 @@ def main():
     )
     t_opt = time.time()
 
-    print('Mapping finished')
-    print(f'Total time elapsed [s]: {t_opt - t_start}\n\n')
+    outputs = [node for node, gtype in lut.nodes(data='gtype') if gtype == gate.PO]
+    inputs = [node for node, gtype in lut.nodes(data='gtype') if gtype == gate.PI]
+
+    max_depth = 0
+    for input in inputs:
+        for output in outputs:
+            try:
+                crit_path = max(nx.all_simple_paths(lut, input, output), key=len)
+                depth = len(crit_path) - 1
+                if depth > max_depth:
+                    max_depth = depth
+            except:
+                pass
+
     print(
+        'Mapping finished\n'
+        f'Total time elapsed [s]: {t_opt - t_start}\n\n'
         f'Time [s] needed for\n'
         f'\tBNG creation: {t_bng - t_start}\n'
         f'\tLabel Phase: {t_label - t_bng}\n'
         f'\tMapping Phase: {t_flowmap - t_label}\n'
         f'\tOptimisations: {t_opt - t_flowmap}\n\n'
-    )
-    print(
+
+        f'Depth: {max_depth}\n\n'
         f'LUT counts:\n'
         f'\tPre optimisation: {lut_count_pre_opt}\n'
         f'\tPost optimisation: {lut_count_post_opt}\n'
